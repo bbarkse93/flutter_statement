9ec5e670dc3a4cc1208ba3086b28794b1df7cbc7
bbarkse93 <bbarkse1129@gmail.com>
2024-01-02 11:13:39 +0900

    StatefulWidget

--- lib/stateful_widget/f_cart.dart
+++ lib/stateful_widget/f_cart.dart
@@ -1,10 +1,58 @@
+import 'package:class_statement/common/models/vo_catalog.dart';
+import 'package:class_statement/common/w_catalog_item.dart';
 import 'package:flutter/material.dart';
 
 class CartWidget extends StatelessWidget {
-  const CartWidget({super.key});
+  // 선택된 카탈로그 정보를 받고 콜백 함수까지 전달 받아야 함
+  final List<Catalog> cartList;
+  final void Function(Catalog catalog) onPressedCatalog;
+
+  const CartWidget({
+    super.key,
+    required this.cartList,
+    required this.onPressedCatalog,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Column(
+      children: [
+        // 카탈로그 아이템
+        Expanded(
+          flex: 5,
+          child: ListView.builder(
+            itemCount: cartList.length,
+            itemBuilder: (context, index) {
+              // 하나의 오브젝트만 뽑자
+              Catalog catalog = cartList[index];
+              return CatalogItem(
+                  catalog: catalog,
+                  isInCart: true,
+                  onPressedCatalog: onPressedCatalog);
+            },
+          ),
+        ),
+        // 구분선
+        Divider(
+          height: 1,
+          thickness: 1,
+          color: Colors.green,
+        ),
+        Expanded(
+          flex: 1,
+          child: Center(
+            child: Text(
+              'SUM : ${cartList.length}',
+              style: TextStyle(
+                fontSize: 22.0,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        )
+
+        // 합계 - 텍스트
+      ],
+    );
   }
 }
--- lib/stateful_widget/s_home_1.dart
+++ lib/stateful_widget/s_home_1.dart
@@ -11,11 +11,39 @@ class HomeScreen1 extends StatefulWidget {
 }
 
 class _HomeScreen1State extends State<HomeScreen1> {
-
   // 현재 선택된 index
   int currentIndex = 0;
+
   // 선택된 catalog 정보를 담을 수 있는 자료구조 선언
-  List<Catalog> catalogList = [];
+  List<Catalog> catalogList = [
+    Catalog.name(color: Colors.red, title: "테스트 1"),
+    Catalog.name(color: Colors.orange, title: "테스트 2")
+  ];
+
+  // 샘플 데이터 (local DB, 통신)
+  late List<Catalog> responseListData;
+
+  // 메모리에 올라갈 때 딱 한번만 초기화 되는 부분
+  @override
+  void initState() {
+    super.initState();
+    // initState <-- 위젯을 업데이트 하는 코드를 사용하면 안됨(버그 발생)
+    // 가능한 한 데이터 초기화 처리만 사용
+    responseListData = catalogListSample;
+  }
+
+  // 콜백 받을 메서드 선언
+  void onPressedCatalog(Catalog catalog) {
+    // 콜백이 일어나면 UI 업데이트 처리
+    print("여기 HomeScreen 위젯 이벤트 발생");
+    setState(() {
+      if (catalogList.contains(catalog)) {
+        catalogList.remove(catalog);
+      } else {
+        catalogList.add(catalog);
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -24,10 +52,15 @@ class _HomeScreen1State extends State<HomeScreen1> {
         centerTitle: true,
         title: const Text("My Catalog"),
       ),
-      body: const IndexedStack(
+      body: IndexedStack(
         children: [
-          CatalogWidget(),
-          CartWidget(),
+          // CatalogWidget(
+          //     responseListData: responseListData,
+          //     cartCatalogList: catalogList,
+          //     onPressedCatalog: onPressedCatalog),
+          CartWidget(cartList: catalogList,
+            onPressedCatalog: onPressedCatalog,
+          ),
         ],
       ),
     );
